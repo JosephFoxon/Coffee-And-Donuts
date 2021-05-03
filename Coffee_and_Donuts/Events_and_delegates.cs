b29f1236947267403f51b385bf0a711e235ed71e
Joseph Foxon <20118068@vcconnect.co.za>
2021-05-03 13:55:57 +0200

    woot woot

--- Coffee_and_Donuts/Events_and_delegates.cs
+++ Coffee_and_Donuts/Events_and_delegates.cs
@@ -8,11 +8,51 @@ namespace Coffee_and_Donuts
     {
         public delegate bool checkDoneDelegate();
         public delegate int addToOrderDelegate(int i);
+        public int numDonutsOutstanding;
+        public int numDonutsMade = 0;
+        public bool isDonutsDone = false;
+        public int numCoffeeOutstanding;
+        public int numCoffeeMade = 0;
+        public bool isCoffeeDone = false;
+        public bool isDone = false;
 
-        public void delegateWorker()
+        public void addDonuts(int i)
         {
-            addToOrderDelegate adder = new addToOrderDelegate()
+            numDonutsOutstanding = i;
         }
-       
+        public void madeDonuts()
+        {
+            numDonutsMade++;
+        }
+        public void checkDonutsIsDone()
+        {
+            if (numDonutsMade >= numDonutsOutstanding)
+            {
+                isDonutsDone = true;
+            }
+        }
+        public void addCoffee(int i)
+        {
+            numCoffeeOutstanding = i;
+        }
+        public void madeCoffee()
+        {
+            numCoffeeMade++;
+        }
+        public void checkCoffeeIsDone()
+        {
+            if (numCoffeeMade >= numCoffeeOutstanding)
+            {
+                isCoffeeDone = true;
+            }
+        }
+        public void checkIsDone()
+        {
+            if((isCoffeeDone==true)&&(isDonutsDone==true))
+            {
+                isDone = true;
+            }
+        }
+               
     }
 }
